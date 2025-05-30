986effb02f9b740bf4bab02896194b70c5d729d9
Yamin <yamin@mac-2.home>
2025-05-30 15:41:55 +0200

    Fix metadata placement issue for Next.js deployment

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 import { AboutHero } from '../../../components/pages/About/AboutHero';
 import { AboutStats } from '../../../components/pages/About/AboutStats';
